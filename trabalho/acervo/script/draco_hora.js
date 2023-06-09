2e892f693bc98fa9ebd59030f398e2b1d2a045be
ansoares14 <136024082+ansoares14@users.noreply.github.com>
2023-06-09 14:15:23 -0300

    agora vai

---
+++ trabalho/acervo/script/draco_hora.js
@@ -0,0 +1,1 @@
+function atualizaRelogio(){var a=new Date,b=a.getHours(),c=a.getMinutes(),d=a.getSeconds();10>b&&(b="0"+b),10>c&&(c="0"+c),10>d&&(d="0"+d),horaFormat=b+" : "+c+" : "+d,document.getElementById("hora").innerHTML=horaFormat,setTimeout("atualizaRelogio()",1e3)}
